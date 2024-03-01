c4084cbbb982fcd47dbc503403ce538e68d26933
Mohamed Ahmed <ahmedma@usc.edu>
2024-02-29 22:44:20 -0800

    llrec

--- llrec.cpp
+++ llrec.cpp
@@ -5,3 +5,31 @@
 //*********************************************
 
 
+void llpivot(Node*& head, Node*& smaller, Node*& larger, int pivot){
+    if (head == NULL){
+        return;
+    }
+    if(head->val > pivot){
+        Node*& headNext = head->next;
+        head->next == NULL;
+        if (larger == NULL){
+            larger == head;
+        }else{
+            larger->next = head;
+        }
+
+        llpivot(headNext, smaller, larger, pivot);
+    }
+
+    if(head->val < pivot){
+        Node*& headNext = head->next;
+        head->next == NULL;
+        if (smaller == NULL){
+            smaller == head;
+        }else{
+            smaller->next = head;
+        }
+
+        llpivot(headNext, smaller, larger, pivot);
+    }
+}
